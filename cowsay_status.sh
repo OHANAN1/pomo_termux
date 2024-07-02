bb63438565845c4fdfaa1f369dc27fb4ec55b089
Loois <loois@pop-os.localdomain>
2024-07-02 02:02:11 +0200

    Adding Tasks is now forced and then displayed

--- cowsay_status.sh
+++ cowsay_status.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+then
+    echo "----------------"
+    echo "No task provided"
+    echo "Usage: $0 <task>"
+    exit
+fi
+
+TASK="$1"
+
 function init_tgui() {
     clear
     toilet -f ivrit --gay "Pomo"
+    echo "Current task: $TASK"
+    echo -e "\n\n\n\n"
 }
 init_tgui
+
+
 
 # Hide the cursor
 function hide_cursor() {
     tput cnorm
 }
 trap hide_cursor EXIT
+
 
 tput civis
 bash ~/Programs/pomo_termux/pomo.sh status | while IFS= read -r line; do
