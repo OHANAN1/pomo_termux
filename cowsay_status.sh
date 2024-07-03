2988e2102c8fe73ad42657a25943c108d1322cc4
Loois <loois@pop-os.localdomain>
2024-07-03 04:14:49 +0200

    More Voice and 4 Cycles at once with input of task.

--- cowsay_status.sh
+++ cowsay_status.sh
@@ -29,7 +29,7 @@ function init_tgui() {
     toilet -f ivrit --gay "Pomo"
     echo "Current task: "
     echo "----------------"
-    echo "  $TASK"
+    echo "  $1"
     echo -e "\n\n\n\n"
 
     # Hide cursor on exit
@@ -37,68 +37,110 @@ function init_tgui() {
     tput civis
 }
 
+# ╒══════════════════════════════════════════════════════════╕
+#                      Utility functions
+# ╘══════════════════════════════════════════════════════════╛
+
+function notify() {
+    termux-notification -t "Pomodoro" -c "$1" --prio high
+    termux-tts-speak "$1"
+    termux-vibrate -d 1000
+}
+
+function exit_pomodoro() {
+    notify "Exiting pomodoro"
+    $PATH_TO_POMO_SCRIPT stop
+
+    clear
+    exit
+}
+
+function get_current_status() {
+  if [ "$1" == "W" ]; then
+    echo "Work "
+  elif [ "$1" == "B" ]; then
+    echo "Break"
+  else
+    echo "Unknown status: $short_status"
+  fi
+
+
+}
 
 # ╒══════════════════════════════════════════════════════════╕
 #                          Main Loop
 # ╘══════════════════════════════════════════════════════════╛
-counter=0
+counter_seconds=0
+counter_pomodoros=0
 old_short_status="W"
 
 $PATH_TO_POMO_SCRIPT start
+notify "Starting pomodoro for task $TASK"
 
-init_tgui
+init_tgui "$TASK"
 
 while true; do
+
+  # Redraw the whole screen every 30 seconds
+  counter_seconds=$((counter_seconds+1))
+  if [ $counter_seconds -eq 30 ]; then
+    init_tgui "$TASK"
+    counter_seconds=0
+  fi
 
   pomodoro_clock=$($PATH_TO_POMO_SCRIPT clock)
   short_status="${pomodoro_clock:1:1}"
   time_left="${pomodoro_clock:2:6}"
 
-  if [ "$short_status" == "W" ]; then
-    current_status="Work "
-  elif [ "$short_status" == "B" ]; then
-    current_status="Break"
-  else
-    echo "Unknown status: $short_status"
-  fi
+  current_status=$(get_current_status "$short_status")
 
   # Save cursor position
   tput sc
 
   # Main output
   echo "Status: $current_status"
-  echo $time_left | cowsay
+  echo "$time_left" | cowsay
 
   # Restore cursor position -> Redraws only one line
   tput rc
 
   sleep 1
 
-  # Redraw the whole screen every 30 seconds
-  counter=$((counter+1))
-  if [ $counter -eq 30 ]; then
-    init_tgui
-    counter=0
-  fi
 
-  # Break script after break so new task can be set
-  # Not final version yet, since this is hacky
+  # When the break is over new cycle starts
   if [ "$short_status" == "W" ] && [ "$old_short_status" == "B" ]; then
-    termux-notification -t "Pomodoro" -c "Break over select new task" --prio high
-    termux-tts-speak "Break over select new task"
-    $PATH_TO_POMO_SCRIPT stop
+    notify "The Break is over, select a new task"
+
+    counter_pomodoros=$((counter_pomodoros+1))
+    # Break every 4 pomodoros
+    if [ $counter_pomodoros -eq 4 ]; then
+        notify "Long break starts now. Bye!"
+        termux-vibrate -d 1000
+        exit_pomodoro
+    fi
 
     clear
+    while true; do
 
-    break
+        # Start continuouse notification
+        notify "Select a new task"
+
+        read -sp "Select new Task: " TASK
+        if [ -z "$TASK" ]; then
+            echo "Task cannot be empty"
+        else
+            break
+        fi
+    done
+    notify "Starting pomodoro for task $TASK"
+
+
   fi
 
   if [ "$short_status" != "$old_short_status" ]; then
     termux-toast "Status changed to $current_status"
+    notify "Status changed to $current_status"
     termux-vibrate -d 1000
-    termux-notification -t "Pomodoro" -c "Status changed to $current_status" --prio high
-    termux-tts-speak "Status changed to $current_status"
-
     old_short_status=$short_status
 
   fi
